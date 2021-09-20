8bc64742b3b20253653df8fdaab8f5606310a8b7
Flone <37961576+Flone-dnb@users.noreply.github.com>
2021-09-20 19:03:09 +0500

    Correctly rerun install.sh.

--- install.sh
+++ install.sh
@@ -6,13 +6,16 @@ sudo chmod +x /usr/local/bin/BloodyPlayer
 
 # Copy FMOD libs.
 if [ ! -d "/usr/local/lib/BloodyPlayer" ]; then
-    sudo mkdir /usr/local/lib/BloodyPlayer/
+    sudo mkdir /usr/local/lib/BloodyPlayer
+else
+    # Remove old libs.
+    sudo rm /usr/local/lib/BloodyPlayer/*
 fi
 sudo cp -Prv ext/FMOD/lib/x64_linux/* /usr/local/lib/BloodyPlayer/
 
 # Copy icon.
 if [ ! -d "/usr/share/pixmaps" ]; then
-    sudo mkdir /usr/share/pixmaps/
+    sudo mkdir /usr/share/pixmaps
 fi
 sudo cp res/bloodyLogo2.png /usr/share/pixmaps/
 
@@ -20,7 +23,14 @@ sudo cp res/bloodyLogo2.png /usr/share/pixmaps/
 if [ ! -d "/etc/ld.so.conf.d" ]; then
     sudo mkdir /etc/ld.so.conf.d
 fi
+if [ -f "/etc/ld.so.conf.d/bloodyplayer.conf" ]; then
+    if [ $(cat /etc/ld.so.conf.d/bloodyplayer.conf) != "/usr/local/lib/BloodyPlayer" ]; then
+        sudo rm /etc/ld.so.conf.d/bloodyplayer.conf
+        echo '/usr/local/lib/BloodyPlayer' | sudo tee -a /etc/ld.so.conf.d/bloodyplayer.conf
+    fi
+else
 echo '/usr/local/lib/BloodyPlayer' | sudo tee -a /etc/ld.so.conf.d/bloodyplayer.conf
+fi
 sudo ldconfig
 
 # Create desktop entry.
